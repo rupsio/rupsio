78d10ce1ab3b07bc1b594c3205f0a0968b04bf62
rupert <jrgarscia@gmail.com>
2024-11-22 12:11:09 +0100

    init

---
+++ apps/dashboard/app/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+          <p>
+            rups.io <code>app</code>
+          </p>
+      </main>
+    </div>
+  );
+}
